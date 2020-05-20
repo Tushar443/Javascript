2690d4bc6313a5655181dff072280e8a47a8c221
Tushar More <moretushart33@gmail.com>
2020-05-20 14:49:41 -0400

    Create server commited

---
+++ CreateServer.js
@@ -0,0 +1,8 @@
+var http =require ('http');
+var server =http.createServer(function(req,resp){
+    resp.end('server Worls!!');
+})
+
+server.listen(3000,function(){
+console.log('Server started on port :3000');
+});
